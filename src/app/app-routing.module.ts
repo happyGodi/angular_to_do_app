1266281b6f609a043dc21bba00ba187fdee161a7
happyGodi <godiramaru0@gmail.com>
2023-03-28 10:43:33 +0300

    feat: landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '', component: LandingComponent }
+  { path: '', title: 'Landing page', component: LandingComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
